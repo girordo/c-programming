d674e8d704813cd5941ae6782bf24ea13f5adb57
girordo <tsgiroldo@gmail.com>
2023-06-12 19:50:49 -0300

    feat(onlinebanking): creating file

--- online-banking/OnlineBanking.c
+++ online-banking/OnlineBanking.c
@@ -14,9 +14,50 @@ int main()
 {
   struct user usr;
 
+  FILE *fp;
+  char filename[50];
+
+  int opt;
+
   printf("\nWhat do you want to do?");
   printf("\n\n1. Register an account");
-  printf("\n2. Login to account");
+  printf("\n2. Login to an account");
+
+  printf("\n\nYour choice: \n");
+  scanf("%d", &opt);
+
+  switch (opt)
+  {
+  case 1:
+    system("clear");
+    printf("Enter your account number:");
+    scanf("%s", usr.ac);
+    printf("\nEnter your phone number:");
+    scanf("%s", usr.phone);
+    printf("\nEnter your new password:");
+    scanf("%s", usr.password);
+    usr.balance = 0;
+    strcpy(filename, usr.phone);
+    fp = fopen(strcat(filename, "dat"), "w");
+    fwrite(&usr, sizeof(struct user), 1, fp);
+
+    if (fwrite != 0)
+    {
+      printf("\n\nAccount succesfully registered");
+    }
+    else
+    {
+      printf("\n\nSomething went wrong please try again");
+    }
+
+  case 2:
+    system("clear");
+    printf("Enter your account number:");
+    scanf("%s", usr.ac);
+
+  default:
+    printf("You have to select one of the options");
+  }
 
   return 0;
 }
